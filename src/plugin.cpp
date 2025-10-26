aea7dec1bc74ceb05b6d2ae55d6e86d4343dd952
Julien Lin <julienlin2003@gmail.com>
2025-10-26 22:49:52 +0100

    adding a function to visualize cfg

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,28 +1,93 @@
 #include <gcc-plugin.h>
+#include <plugin-version.h>
 #include <tree-pass.h>
 #include <context.h>
 #include <tree.h>
 #include <basic-block.h>
 #include <gimple.h>
+#include <function.h>                                                            
 #include <gimple-iterator.h>
+
+
+
+/**************** Generate graphviz **************/
+
+/* Build a filename (as a string) based on function name */
+	static char * 
+cfgviz_generate_filename( function * fun, const char * suffix )
+{
+	char * target_filename ; 
+
+	target_filename = (char *)xmalloc( 2048 * sizeof( char ) ) ;
+
+	snprintf( target_filename, 1024, "%s_%s_%d_%s.dot", 
+			current_function_name(),
+			LOCATION_FILE( fun->function_start_locus ),
+			LOCATION_LINE( fun->function_start_locus ),
+			suffix ) ;
+
+	return target_filename ;
+}
+
+/* Dump the graphviz representation of function 'fun' in file 'out' */
+	static void 
+cfgviz_internal_dump( function * fun, FILE * out ) 
+{
+
+	// Print the header line and open the main graph
+	fprintf(out, "Digraph G{\n");
+
+
+	/*****************************/
+	/***** COMPLETE HERE ********/	
+	/*****************************/
+    basic_block bb;
+
+    FOR_EACH_BB_FN (bb, cfun)
+        {
+            fprintf(out, "N%d [label=\"Basic Block %d\" shape=ellipse]\n", bb->index, bb->index);
+            edge e;
+            edge_iterator ei;
+
+            FOR_EACH_EDGE(e, ei, bb->succs)
+                {   
+                    fprintf(out, "N%d -> N%d [color=red label=\"\"]\n", e->src->index, e->dest->index); 
+                    if(e->flags & EDGE_FALLTHRU)
+                        break;
+                }   
+        }
+
+
+	// Close the main graph
+	fprintf(out, "}\n");
+}
+
+	void 
+cfgviz_dump( function * fun, const char * suffix )
+{
+	char * target_filename ; 
+	FILE * out ;
+
+	target_filename = cfgviz_generate_filename( fun, suffix ) ;
+
+	printf( "[GRAPHVIZ] Generating CFG of function %s in file <%s>\n",
+			current_function_name(), target_filename ) ;
+
+	out = fopen( target_filename, "w" ) ;
+
+	cfgviz_internal_dump( fun, out ) ;
+
+	fclose( out ) ;
+	free( target_filename ) ;
+}
+
+
+/**************** End - Generate graphviz **************/
+
 
 
 int plugin_is_GPL_compatible;
 
-/*
-struct register_pass_info {
-    opt_pass *pass;
-    const char *reference_pass_name;
-    int ref_pass_instance_number;
-    enum pass_positioning_ops pos_op;
-};
-
-enum pass_positioning_ops {
-    PASS_POS_INSERT_AFTER,
-    PASS_POS_INSERT_BEFORE,
-    PASS_POS_REPLACE
-};
-*/
 
 const pass_data my_pass_data = {
     GIMPLE_PASS,
@@ -60,6 +125,7 @@ class my_pass : public gimple_opt_pass {
                     printf("line: %d\n", line);
                 }
             }
+            cfgviz_dump(fun, "cfg");
             return 0;
         }
 };
